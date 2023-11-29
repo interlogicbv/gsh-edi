cd85d75d20ce2f3b5f25567a3bea04280c1ba746
Rutger van Rees <r.vanrees@icloud.com>
2023-11-29 17:03:21 +0100

    First CSV parse attempt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello world!!");
+import * as fs from "fs";
+import { v4 as uuid } from "uuid";
+import { parse } from "csv-parse";
+
+var generatedReference = uuid();
+var index: number = 0;
+const keys: any[] = [];
+const values: any[] = [];
+var results: any[] = [];
+
+const log = (text: string): void => {
+  console.log(`[${new Date().toLocaleString()}] ${text}`);
+};
+
+const start = () => {
+  log(`Running with reference: ${generatedReference}`);
+  fs.readdirSync("./src/input").map((file: any) => {
+    if (file) {
+      log(`Found input file: ${file}`);
+      fs.createReadStream("./src/input/" + file)
+        .pipe(
+          parse({
+            delimiter: ";",
+          })
+        )
+        .on("data", (r: string[]) => {
+          if (index === 0) {
+            keys.push(r);
+          } else {
+            results.push(Object.assign({}, r));
+          }
+          index++;
+        })
+        .on("end", () => {
+          console.log(results);
+        });
+    }
+  });
+};
+
+start();
